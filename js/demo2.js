b56bdea37a54caa279d3fa88c3d9bb7b3ced9b10
JRE-Sun <jresun@163.com>
2017-01-13 16:16:01 +0800

    版本4

--- js/demo2.js
+++ js/demo2.js
@@ -7,6 +7,13 @@ $(function() {
         spaceBetween: 30,
         mousewheelControl: true,
         preventClicks: true,
+        // slidesPerView: 'auto',
+        speed: 400, //移动的速度
+        loop: true, //是够能够循环无缝滑动
+        grabCursor: true,
+        paginationClickable: true, //允许触摸
+        // autoplay: 4000, //一秒切换
+        observeParents: true,
         onInit: function(swiper) { //Swiper2.x的初始化是onFirstInit
             swiperAnimateCache(swiper); //隐藏动画元素 
             swiperAnimate(swiper); //初始化完成开始动画
